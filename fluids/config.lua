4d133c3e0f7f90cdb8dcbca142a68f0819a787de
Luis Arias <891590+luisantonioa@users.noreply.github.com>
2025-05-17 22:37:09 -0700

    Update config.lua

--- fluids/config.lua
+++ fluids/config.lua
@@ -1,9 +1,11 @@
-return {
-  interfaceAddress = "bea7e9dc-bc7f-4b4b-88dc-8d65b08df7b0",
-  displayEntries = 10,
-  loopInterval = 60,
-  debugMode = true,
-  maxLogEntries = 1000,
-  thresholdFile = "/home/fluid_thresholds.lua",
-  logFile = "/home/craft_log.lua"
-}
+local config = {}
+
+config.DISPLAY_ENTRIES = 10
+config.LOOP_INTERVAL = 60  -- seconds
+config.DEBUG_MODE = true
+config.LOG_FILE = "/home/fluids/craft_log.lua"
+config.THRESHOLD_FILE = "/home/fluids/thresholds.lua"
+config.MAX_LOG_ENTRIES = 1000
+config.INTERFACE_ADDRESS = "bea7e9dc-bc7f-4b4b-88dc-8d65b08df7b0"
+
+return config
