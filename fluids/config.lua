be14698b95cee248aad7ee513967f0374ecfca40
Luis Arias <891590+luisantonioa@users.noreply.github.com>
2025-05-17 22:32:28 -0700

    Create config.lua

---
+++ fluids/config.lua
@@ -0,0 +1,9 @@
+return {
+  interfaceAddress = "bea7e9dc-bc7f-4b4b-88dc-8d65b08df7b0",
+  displayEntries = 10,
+  loopInterval = 60,
+  debugMode = true,
+  maxLogEntries = 1000,
+  thresholdFile = "/home/fluid_thresholds.lua",
+  logFile = "/home/craft_log.lua"
+}
