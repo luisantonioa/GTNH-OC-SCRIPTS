3f40f7fba20bad7f08b843d3b8f7b17eb0024bac
Luis Arias <891590+luisantonioa@users.noreply.github.com>
2025-05-19 12:22:24 +1000

    update

--- fluid_thresholds.lua
+++ fluid_thresholds.lua
@@ -1,4 +1,7 @@
+local k, M, B = 1000, 1000000, 1000000000
 return {
-  ["Chlorine"] = {lower = 50000000, upper = 100000000},
-  ["Oxygen"] = {lower = 300000, upper = 1000000},
+  ["Chlorine"] = {lower = 180 * M, upper = 200 * M},
+  ["Fluorine"] = {lower = 4 * M, upper = 5 * M},
+  ["Hydrochloric Acid"] = {lower = 10 * M, upper = 20 * M},
+  ["Oxygen Gas"] = {lower = 30 * M, upper = 100 * M},
 }
